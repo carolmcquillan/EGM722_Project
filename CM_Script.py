e27f742bb25fe363cd88c6e0d015dc1515cbed7b
carolmcquillan <carolmcquillan@hotmail.com>
2023-04-21 11:59:19 +0100

    BufferCode
    Buffer Not yet working need to define dataframe

--- CM_Script.py
+++ CM_Script.py
@@ -1,21 +1,21 @@
-#- - - - - - - - - - - - - - -  Import required modules/packages/dependencies- - - - - - - - - - - - - -  
+#- - - - - - - - - - - - - - -  Import required modules/packages/dependencies- - - - - - - - - - - - - - - - - - - - - _
 import pandas as pd
 import geopandas as gpd
 import folium
 import ipywidgets
 from IPython.display import HTML, display
  
-#- - - - - - - - - - - - - - -  Add initial data- - - - - - - - - - - - - -     
+#- - - - - - - - - - - - - - -  Add initial data- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -- - - -
 # This adds the bike locations we will be working with 
 locations = ("https://www.belfastcity.gov.uk/getmedia/f68a4e53-68df-429c-8ac8-a8be9411ab73/belfast-bike-stations-updated-25-june-2021.csv")
 bike_station_locations= pd.read_csv(locations)
-
-#- - - - - - - - - - - - - - -  Create the map- - - - - - - - - - - - - -  
+#TODO: add additional layers
+#- - - - - - - - - - - - - - -  Create the map- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 #this creates the map and centres the map on the mean x,y of all bike station locations
 map = folium.Map(location=[bike_station_locations.Latitude.mean(), bike_station_locations.Longitude.mean()], zoom_start=14, control_scale=True)
 
 
-#- - - - - - - - - - - - - - -  Add data to the map- - - - - - - - - - - - - -  
+#- - - - - - - - - - - - - - -  Add data to the map- - - - - - - - - - - - - -- - - - - - - - - - - - - - - - - - - - -
 #this adds the bike station point locations to the map and uses the location field as a popup label
 for index, row in bike_station_locations.iterrows():
     lat = row['Latitude']
@@ -27,14 +27,21 @@ for index, row in bike_station_locations.iterrows():
     
 # add data layers control
 folium.LayerControl().add_to(map)
+#TODO: configure widget
+#- - - - - - - - - - - - - - -  Carry out spatial analyses- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+oneKmBuffer = bike_station_locations.geometry.buffer(1)
+oneKmBuffer.head()
 
-#- - - - - - - - - - - - - - -  Carry out spatail analyses- - - - - - - - - - - - - -  
+#for index, row in bike_station_locations.iterrows():
 
 
-#- - - - - - - - - - - - - - -  Present results from analysis- - - - - - - - - - - - - -  
 
+#TODO: define and code for further analyses
 
-#- - - - - - - - - - - - - - - Finishing Touches- - - - - - - - - - - - - - - 
+#- - - - - - - - - - - - - - -  Present results from analysis- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#TODO: present results
+
+#- - - - - - - - - - - - - - - Finishing Touches- - - - - - - - - - - - - - -- - - - - - - - - - - - - - - - - - - - - -
 #this adds a title to the map   
 mapTitle = 'Belfast Bike Locations'
 title_html = '''
@@ -46,9 +53,9 @@ map.get_root().html.add_child(folium.Element(title_html))
 
 
 
-#- - - - - - - - - - - - - - - Save the map- - - - - - - - - - - - - - - 
+#- - - - - - - - - - - - - - - Save the map- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 #this saves the map as html
-map.save("Belfastbikes.html")
+map.save("Belfast bikes.html")
 
 
 
